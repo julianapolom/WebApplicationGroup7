1c79a84cfcaf11bfe32e198a90330e8e3f4eb34f
Esteban Alonso Concha Rojas <econcha@corvatech.com>
2023-02-26 13:51:38 -0500

    ✨ Add new homecontroller

---
+++ WebApplicationIntcomex/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationIntcomex.Models;
+
+namespace WebApplicationIntcomex.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Client()
+        {
+            return RedirectToAction("Index", "Client");
+        }
+    }
+}
